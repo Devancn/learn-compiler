f641f02c32a993dc5d2800cc61f4ff4b2f890eab
dingxiaohuan <wydingxiaohuan@163.com>
2021-01-27 20:28:27 +0800

    ast traverse

--- compiler.js
+++ compiler.js
@@ -109,7 +109,77 @@ function parser(tokens) {
   }
   return ast;
 }
+
+function traverser(ast, visitor) {
+  function traverseArray(array, parent) {
+    array.forEach(function(child) {
+      traverseNode(child, parent)
+    })
+  }
+
+  function traverseNode(node, parent) {
+    const method = visitor[node.type];
+    if(method) {
+      method(node, parent);
+    }
+
+    switch (node.type) {
+      case 'Program':
+        traverseArray(node.body, node);
+        break;
+      case 'CallExpression': 
+        traverseArray(node.params, node);
+        break;
+      case 'NumberLiteral':
+        break;
+      default:
+        throw new TypeError(node.type)
+    }
+
+  }
+  traverseNode(ast, null)
+}
+
+function transformer(ast) {
+  debugger
+  const newAst = {
+    type: 'Program',
+    body: []
+  }
+
+  ast._context = newAst.body;
+  traverser(ast, {
+    NumberLiteral: function(node, parent) {
+      parent._context.push({
+        type: 'NumberLiteral',
+        value: node.value
+      })
+    },
+    CallExpression: function(node, parent) {
+      let expression = {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: node.name
+        },
+        arguments: []
+      }
+      node._context = expression.arguments;
+      if(parent.type !== 'CallExpression') {
+        expression = {
+          type: 'ExpressionStatement',
+          expression
+        }
+      }
+
+      parent._context.push(expression);
+    }
+  })
+
+  return newAst;
+}
 module.exports = {
   tokenizer,
-  parser
+  parser,
+  transformer
 };
